0b9e54d1aaeee8d39bf42a0c5caeb1fb02d9ebe9
Justin Drake <drakefjustin@gmail.com>
2013-02-22 18:02:18 +0000

    Worked on the focused entry

---
+++ client/js/content.js
@@ -0,0 +1,48 @@
+var policy_domains = [
+    'agriculture',
+    'health'
+];
+
+var items = [
+    {
+        // Basic
+        title: 'Icecream for Europe',
+        url: 'www.icecream.eu',
+        contacts: [
+            {
+                name: 'Maxime Beylot',
+                position: 'chairman',
+                email: 'm.beylot@hotmail.fr',
+            },
+            {
+                name: 'Pierre Dagard',
+                position: 'cochairman',
+                email: 'pierre.dagard@gmail.com'
+            }
+        ],
+        domain: [
+            'youth',
+            'health'
+        ], // From policy_domains
+        purpose: '', // What is the ICE about?
+        motives: '', // Why justifies the ICE?
+
+        // Content
+        image: 'icecream.jpg',
+        video: '',
+        text: '',
+        comments: [],
+
+        // Data
+        votes: '123',
+        start: '',
+
+    },
+    {
+        title: 'Barklay',
+        paragraph: 'Test2'
+        image: ,
+        progress: ,
+        start: ,
+    },
+];
